47644b2b6837841d7fbe1e15728cce883b71c5ba
ck <ck@ck.local>
2024-11-15 09:07:03 +0530

    upload file

---
+++ upload_to_s3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Variables
+S3_BUCKET="digital-ads"
+FILE_PATH="/Users/ck/gitrepos-demo/b25_repo/file.txt"
+S3_DESTINATION="s3://$S3_BUCKET/dev/file.txt"
+
+# Upload file to S3 bucket
+echo "Uploading $FILE_PATH to $S3_DESTINATION..."
+aws s3 cp "$FILE_PATH" "$S3_DESTINATION"
+
+# Check if the upload was successful
+if [ $? -eq 0 ]; then
+    echo "File successfully uploaded to S3."
+else
+    echo "Error uploading file to S3."
+fi
